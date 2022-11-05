285ebc8f512185c348e27c2a527b1cd148a57da9
coco sk <corentinwat@free.fr>
2022-11-05 18:49:22 +0100

    notification + chgt de logo

--- app/src/main/java/fr/sharkos/mealready/MainActivity.java
+++ app/src/main/java/fr/sharkos/mealready/MainActivity.java
@@ -1,10 +1,17 @@
 package fr.sharkos.mealready;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import static android.content.ContentValues.TAG;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -34,7 +41,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button clickToEat = findViewById(R.id.clickToEat);
+        Button clickNotif= findViewById(R.id.clickNotif);
 
+        // Bouton pour appeler l'API
         clickToEat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -43,7 +52,34 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(listPlatIntent);
             }
         });
+
+        // Bouton pour afficher la notification
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("MealReady Notification", "MealReady Notification",NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+
+        clickNotif.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Nouveauté \uD83D\uDC9B", Toast.LENGTH_SHORT).show();
+                addNotification();
+            }
+        });
     }
 
+    private void addNotification() {
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this, "MealReady Notification");
+        builder.setContentTitle("Nouveauté");
+        builder.setContentText("Frite party chez MealReady !!!");
+        builder.setSmallIcon(R.drawable.logo);
+        builder.setAutoCancel(false);
+
+        NotificationManagerCompat managerCompat = NotificationManagerCompat.from(MainActivity.this);
+        managerCompat.notify(1, builder.build());
+
+    }
 
 }
